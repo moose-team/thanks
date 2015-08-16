e117170ef051d41f72abba89bdb2eecfafb732af
Karissa McKelvey <krmckelv@gmail.com>
2015-08-16 20:13:51 +0200

    add app.js

---
+++ app.js
@@ -0,0 +1,16 @@
+var menubar = require('menubar')
+var ipc = require('ipc')
+
+var mb = menubar({
+  dir: __dirname,
+  width: 300,
+  height: 480
+})
+
+mb.on('ready', function ready () {
+  console.log('ready')
+})
+
+ipc.on('terminate', function terminate (ev) {
+  mb.app.terminate()
+})
